10b4ad93ee8af54b9efb3d313f30fc728130378b
Joshué Agapé <joshueagape@gmail.com>
2024-08-01 01:12:24 +0300

    ajout et confirmation d'invitation fini

--- lib/components/my_demande.dart
+++ lib/components/my_demande.dart
@@ -4,31 +4,74 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:ndao_hitafa/components/my_invitations.dart';
 
-class MyDemande extends StatelessWidget {
+class MyDemande extends StatefulWidget {
   final int userId;
   final String label;
+
   const MyDemande({
-    super.key,
+    Key? key,
     required this.label,
     required this.userId,
-  });
+  }) : super(key: key);
+
+  @override
+  _MyDemandeState createState() => _MyDemandeState();
+}
+
+class _MyDemandeState extends State<MyDemande> {
+  late Future<List<dynamic>> contactsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    contactsFuture = fetchContacts();
+  }
 
   Future<List<dynamic>> fetchContacts() async {
-    final response = await http.get(
-      Uri.parse('http://192.168.56.1:7576/api/users/suggestion-amis/$userId'),
-      headers: {'Content-Type': 'application/json'},
-    );
+    try {
+      final response = await http.get(
+        Uri.parse(
+            'http://192.168.56.1:7576/api/users/suggestion-amis/${widget.userId}'),
+        headers: {'Content-Type': 'application/json'},
+      );
 
-    final response2 = await http.get(
-      Uri.parse(
-          'http://192.168.56.1:7576/api/users/lists-invitations-friend/$userId'),
-      headers: {'Content-Type': 'application/json'},
-    );
+      final response2 = await http.get(
+        Uri.parse(
+            'http://192.168.56.1:7576/api/users/lists-invitations-friend/${widget.userId}'),
+        headers: {'Content-Type': 'application/json'},
+      );
 
-    if (label == "Confirmer") {
-      return jsonDecode(response2.body);
-    } else {
-      return jsonDecode(response.body);
+      if (widget.label == "Confirmer") {
+        return jsonDecode(response2.body);
+      } else {
+        return jsonDecode(response.body);
+      }
+    } catch (e) {
+      throw Exception('Failed to load contacts: $e');
+    }
+  }
+
+  Future<void> ajouter(int receivedBy) async {
+    try {
+      await http.post(
+        Uri.parse(
+            'http://192.168.56.1:7576/api/users/add-new-friend/${widget.userId}/$receivedBy'),
+        headers: {'Content-Type': 'application/json'},
+      );
+    } catch (e) {
+      throw Exception('Failed to add friend: $e');
+    }
+  }
+
+  Future<void> confirmer(int invitationId) async {
+    try {
+      await http.put(
+        Uri.parse(
+            'http://192.168.56.1:7576/api/users/accept-a-invitation/${widget.userId}/$invitationId'),
+        headers: {'Content-Type': 'application/json'},
+      );
+    } catch (e) {
+      throw Exception('Failed to confirm invitation: $e');
     }
   }
 
@@ -39,7 +82,7 @@ class MyDemande extends StatelessWidget {
       width: width,
       height: double.infinity,
       child: FutureBuilder<List<dynamic>>(
-        future: fetchContacts(),
+        future: contactsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -57,8 +100,10 @@ class MyDemande extends StatelessWidget {
                   imgUrl: contact['profile_url'] ?? 'default_image_url',
                   username: contact['username'],
                   email: contact['email'],
-                  label: label,
-                  onTap: () {},
+                  label: widget.label,
+                  onTap: widget.label == "Confirmer"
+                      ? () => confirmer(contact['id'])
+                      : () => ajouter(contact['user_id']),
                 );
               },
             );
